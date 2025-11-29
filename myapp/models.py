aeab5e5dc67acb8eeec4699e59b64d1b1f2adcd1
KortDasten <kortdasten@gmail.com>
2025-11-30 03:48:30 +0800

    Final: Applied migrations and pushed Pillow requirement

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
+# myapp/models.py
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    bio = models.TextField()
+    
+    # 🔴 ITO ANG IMPORTANTE: ImageField para sa pictures
+    # Gagamitin nito ang MEDIA_ROOT na ni-set natin sa settings.py
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+    github_link = models.URLField(max_length=200, blank=True)
+    
+    def __str__(self):
+        return self.name
