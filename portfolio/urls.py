5d0623d1229fffb9cfd05f317bc88382558a9644
KortDasten <kortdasten@gmail.com>
2025-11-30 03:05:31 +0800

    Final Fix: Confirmed STATIC_ROOT and MEDIA setup

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,3 +26,4 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
