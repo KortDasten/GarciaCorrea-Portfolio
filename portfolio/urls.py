773829c7aca5281a0923b8d20b3549a118b70e01
KortDasten <kortdasten@gmail.com>
2025-11-30 03:23:30 +0800

    ULTIMATE FIX 2: Corrected urls.py module import error

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,14 +1,15 @@
+# portfolio/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings 
-from django.conf.urls.static import static 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # I assume ang sumusunod ay 'myapp.urls'
     path('', include('myapp.urls')),
 ]
 
-# 🔴 FIX 5: I-force ang Static/Media Files sa Production
-# Tiyakin na ito ay HINDI NAKA-SAKOP NG 'if settings.DEBUG:'
+# Force Static/Media Files
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
