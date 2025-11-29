07a3f0853d3af6b0bc2acbfc31882db5d860e63e
KortDasten <kortdasten@gmail.com>
2025-11-30 03:00:35 +0800

    Fix: Final STATIC_ROOT configuration for Render

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('', include('myapp.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
